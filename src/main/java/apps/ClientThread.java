772165325e65733531ae255473bc7ba4fd78fa7d
acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
2019-09-21 22:39:37 -0500

    organizar codigo

--- src/main/java/apps/ClientThread.java
+++ src/main/java/apps/ClientThread.java
@@ -5,8 +5,6 @@ import java.io.*;
 import java.net.Socket;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import net.sf.image4j.codec.ico.ICODecoder;
 import net.sf.image4j.codec.ico.ICOEncoder;
@@ -23,12 +21,12 @@ import net.sf.image4j.codec.ico.ICOEncoder;
  */
 class ClientThread implements Runnable {
 
-    private static Socket clientSocket;
-    private static PrintWriter out;
-    private static BufferedReader in;
-    private static BufferedOutputStream salidaDatos;
-    private static String RUTA_RESOURCES = "src/main/resources";
-    private static Service service;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private BufferedOutputStream salidaDatos;
+    private String RUTA_RESOURCES = "src/main/resources";
+    private Service service;
 
     ClientThread(Socket clientSocket, Service service) {
         this.clientSocket = clientSocket;
@@ -38,17 +36,17 @@ class ClientThread implements Runnable {
     public void run() {
         try {
             System.out.println("Listo para recibir. Escuchando puerto ");
-            while (!clientSocket.isClosed()) {
+            while (!this.clientSocket.isClosed()) {
                 // El in y el out son para el flujo de datos por el socket (streams).
-                out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()),
+                this.out = new PrintWriter(new OutputStreamWriter(this.clientSocket.getOutputStream()),
                         true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                salidaDatos = new BufferedOutputStream(clientSocket.getOutputStream()); // Muestra los datos respuesta al cliente.
+                this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+                this.salidaDatos = new BufferedOutputStream(this.clientSocket.getOutputStream()); // Muestra los datos respuesta al cliente.
                 processRequest();
             }
-            out.close();
-            in.close();
-            clientSocket.close();
+            this.out.close();
+            this.in.close();
+            this.clientSocket.close();
 
         } catch (IOException e) {
             System.err.println("Accept failed.");
@@ -56,24 +54,24 @@ class ClientThread implements Runnable {
         }
     }
 
-    public static void processRequest() throws IOException {
+    public void processRequest() throws IOException {
         String inputLine, solicitud = "";
-        while ((inputLine = in.readLine()) != null) {
+        while ((inputLine = this.in.readLine()) != null) {
             System.out.println("Received: " + inputLine);
             if (inputLine.contains("GET")) {
                 solicitud = inputLine; // Lee la primera linea de la solicitud
                 System.out.println("Solicitud: " + solicitud); //Ejemplo: GET / HTTP/1.1
                 readRequest(solicitud);
             }
-            if (!in.ready()) { //Ready devuelve verdadero si la secuencia está lista para ser leída.
+            if (!this.in.ready()) { //Ready devuelve verdadero si la secuencia está lista para ser leída.
                 break;
             }
         }
-        out.close();
-        salidaDatos.flush();
+        this.out.close();
+        this.salidaDatos.flush();
     }
 
-    public static void readRequest(String solicitud) throws IOException {
+    public void readRequest(String solicitud) throws IOException {
         StringTokenizer tokens = new StringTokenizer(solicitud); // Divide la solicitud en diferentes "tokens" separados por espacio.
         String metodo = tokens.nextToken().toUpperCase(); // Obtenemos el primer token, que en este caso es el metodo de
         // la solicitud HTTP.
@@ -89,7 +87,7 @@ class ClientThread implements Runnable {
         }
     }
 
-    public static void searchFilesInApps(String archivo) throws IOException {
+    public void searchFilesInApps(String archivo) throws IOException {
         System.out.println("BUSCANDO ARCHIVOS EN APPS");
         boolean useParam = false;
         String parametro = "";
@@ -102,10 +100,10 @@ class ClientThread implements Runnable {
         }
         try {
             String response = service.search(archivo, useParam, parametro);
-            out.println("HTTP/1.1 200 OK\r");
-            out.println("Content-Type: text/html\r");
-            out.println("\r");
-            out.println(response + "\r");
+            this.out.println("HTTP/1.1 200 OK\r");
+            this.out.println("Content-Type: text/html\r");
+            this.out.println("\r");
+            this.out.println(response + "\r");
         } catch (ExceptionServer ex) {
             searchFilesInStaticResources("/notFound.html");
 
@@ -126,7 +124,7 @@ class ClientThread implements Runnable {
         }
     }
 
-    public static void searchFilesInStaticResources(String archivo) throws IOException {
+    public void searchFilesInStaticResources(String archivo) throws IOException {
         System.out.println("SIguibuscando la imagen not found " + archivo);
         BufferedReader br = null;
         if (archivo.equals("/")) {
@@ -135,7 +133,7 @@ class ClientThread implements Runnable {
         String path = RUTA_RESOURCES + archivo;
         try {
             br = new BufferedReader(new FileReader(path));
-            out.println("HTTP/1.1 202 Ok\r");
+            this.out.println("HTTP/1.1 202 Ok\r");
         } catch (Exception e) {
             System.out.println("No lo encontro");
             StringBuffer sb = new StringBuffer();
@@ -145,15 +143,15 @@ class ClientThread implements Runnable {
                     sb.append(infile);
                 }
             }
-            out.println("HTTP/1.1 404 Not Found\r");
-            out.println("Content-Type: text/html\r");
-            out.println("\r");
-            out.println(sb.toString());
+            this.out.println("HTTP/1.1 404 Not Found\r");
+            this.out.println("Content-Type: text/html\r");
+            this.out.println("\r");
+            this.out.println(sb.toString());
         }
 
         if (archivo.contains("jpg")) {
-            out.println("Content-Type: image/jpeg\r");
-            out.println("\r");
+            this.out.println("Content-Type: image/jpeg\r");
+            this.out.println("\r");
             BufferedImage image = ImageIO.read(new File(RUTA_RESOURCES + archivo));
             ImageIO.write(image, "JPG", clientSocket.getOutputStream());
         } else if (archivo.contains("html")) {
@@ -165,14 +163,14 @@ class ClientThread implements Runnable {
                     sb.append(infile);
                 }
             }
-            out.println("Content-Type: text/html\r");
-            out.println("\r");
-            out.println(sb.toString());
+            this.out.println("Content-Type: text/html\r");
+            this.out.println("\r");
+            this.out.println(sb.toString());
 
         } else if (archivo.contains("favicon.ico")) {
             System.out.println("Solicitud de Favicon");
-            out.println("Content-Type: image/x-icon\r");
-            out.println("\r");
+            this.out.println("Content-Type: image/x-icon\r");
+            this.out.println("\r");
             List<BufferedImage> images = ICODecoder.read(new File(RUTA_RESOURCES + archivo));
             ICOEncoder.write(images.get(0), clientSocket.getOutputStream());
         }
